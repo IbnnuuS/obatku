789f2b8d7fa43bf23a6f9b540a9959807d86149e
IbnnuuS <ibnuarmor2@gmail.com>
2025-05-22 16:34:24 +0700

    [BAB 9] NOTIFICATIONS

--- src/screens/addObat/index.jsx
+++ src/screens/addObat/index.jsx
@@ -1,18 +1,9 @@
-// src/screens/addObat/index.jsx
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Alert,
-  ScrollView,
-  Image,
-} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {createObat} from '../../utility';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ScrollView, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { createObat } from '../../utility';
 import ImagePicker from 'react-native-image-crop-picker';
+import notifee, { AndroidImportance } from '@notifee/react-native';
 
 const AddObat = () => {
   const navigation = useNavigation();
@@ -21,13 +12,33 @@ const AddObat = () => {
   const [description, setDescription] = useState('');
   const [image, setImage] = useState('');
   const [selectedCategory, setSelectedCategory] = useState(null);
+  
+  // Data untuk opsi upload
+  const [uploadOption, setUploadOption] = useState('immediate');
 
   const categories = [
-    {id: 1, name: 'Obat'},
-    {id: 2, name: 'Vitamin'},
-    {id: 3, name: 'Herbal'},
-    {id: 4, name: 'Suplemen'},
+    { id: 1, name: 'Obat' },
+    { id: 2, name: 'Vitamin' },
+    { id: 3, name: 'Herbal' },
+    { id: 4, name: 'Suplemen' },
   ];
+
+  // Fungsi untuk memulai setup Notifee (meminta izin dan membuat channel)
+  useEffect(() => {
+    async function setupNotifee() {
+      // Pastikan channel hanya dibuat sekali di awal aplikasi
+      await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+        importance: AndroidImportance.HIGH, // Menentukan pentingnya notifikasi
+      });
+
+      // Meminta izin untuk menampilkan notifikasi
+      await notifee.requestPermission();
+    }
+
+    setupNotifee();
+  }, []);
 
   const handleImagePick = () => {
     ImagePicker.openPicker({
@@ -44,7 +55,26 @@ const AddObat = () => {
       });
   };
 
-  const handleSubmit = async () => {
+  // Fungsi untuk mengirimkan notifikasi
+  const sendNotification = async (message) => {
+    await notifee.displayNotification({
+      title: 'ObatKu Notification',
+      body: message,
+      android: {
+        channelId: 'default',
+      },
+    });
+  };
+
+  // Fungsi untuk menangani penundaan pengiriman obat
+  const handleDelayedPost = (delay) => {
+    setTimeout(async () => {
+      await sendNotification(`Obat akan segera ditambahkan setelah penundaan ${delay} detik.`);
+    }, delay * 1000); // Mengonversi detik ke milidetik
+  };
+
+  // Fungsi untuk mengupload obat
+  const handleUpload = async () => {
     if (!title || !description || !image || !selectedCategory) {
       Alert.alert('Input Error', 'Harap lengkapi semua data!');
       return;
@@ -62,6 +92,7 @@ const AddObat = () => {
       const result = await createObat(newObat);
       if (result) {
         console.log('Data berhasil ditambahkan ke Firestore:', result);
+        await sendNotification('Obat berhasil ditambahkan!');
         Alert.alert('Success', 'Obat berhasil ditambahkan!', [
           {
             text: 'OK',
@@ -73,70 +104,105 @@ const AddObat = () => {
       }
     } catch (error) {
       console.error('Terjadi kesalahan:', error);
+      await sendNotification('Gagal menambahkan obat!');
       Alert.alert('Error', 'Gagal menambahkan obat!');
+    }
+
+    // Mengatur penundaan notifikasi jika ada
+    if (uploadOption === 'delayed10') {
+      handleDelayedPost(10); // Penundaan 10 detik
+    } else if (uploadOption === 'delayed30') {
+      handleDelayedPost(30); // Penundaan 30 detik
     }
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.label}>Nama Obat :</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nama Obat"
-        value={title}
-        onChangeText={setTitle}
-      />
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 20 }} style={styles.container}>
+        <Text style={styles.label}>Nama Obat :</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nama Obat"
+          value={title}
+          onChangeText={setTitle}
+        />
 
-      <Text style={styles.label}>Deskripsi Obat :</Text>
-      <TextInput
-        style={[styles.input, {height: 100}]}
-        placeholder="Deskripsi"
-        value={description}
-        onChangeText={setDescription}
-        multiline
-        numberOfLines={4}
-      />
+        <Text style={styles.label}>Deskripsi Obat :</Text>
+        <TextInput
+          style={[styles.input, { height: 100 }]}
+          placeholder="Deskripsi"
+          value={description}
+          onChangeText={setDescription}
+          multiline
+          numberOfLines={4}
+        />
 
-      <Text style={styles.label}>Gambar :</Text>
-      <TouchableOpacity style={styles.imagePicker} onPress={handleImagePick}>
-        <Text style={styles.imagePickerText}>Pilih Gambar</Text>
-      </TouchableOpacity>
-      {image ? (
-        <Image source={{uri: image}} style={styles.previewImage} />
-      ) : null}
+        <Text style={styles.label}>Gambar :</Text>
+        <TouchableOpacity style={styles.imagePicker} onPress={handleImagePick}>
+          <Text style={styles.imagePickerText}>Pilih Gambar</Text>
+        </TouchableOpacity>
+        {image ? (
+          <Image source={{ uri: image }} style={styles.previewImage} />
+        ) : null}
 
-      <Text style={styles.label}>Kategori :</Text>
-      <View style={styles.categoryContainer}>
-        {categories.map(category => (
-          <TouchableOpacity
-            key={category.id}
-            style={[
-              styles.categoryButton,
-              selectedCategory?.id === category.id && styles.selectedCategory,
-            ]}
-            onPress={() => setSelectedCategory(category)}>
-            <Text
+        <Text style={styles.label}>Kategori :</Text>
+        <View style={styles.categoryContainer}>
+          {categories.map(category => (
+            <TouchableOpacity
+              key={category.id}
               style={[
-                styles.categoryButtonText,
-                selectedCategory?.id === category.id && styles.selectedText,
-              ]}>
-              {category.name}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+                styles.categoryButton,
+                selectedCategory?.id === category.id && styles.selectedCategory,
+              ]}
+              onPress={() => setSelectedCategory(category)}
+            >
+              <Text
+                style={[
+                  styles.categoryButtonText,
+                  selectedCategory?.id === category.id && styles.selectedText,
+                ]}
+              >
+                {category.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
 
-      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-        <Text style={styles.submitButtonText}>Submit</Text>
-      </TouchableOpacity>
-    </ScrollView>
+        <Text style={styles.label}>Pilih Opsi Penundaan Notifikasi:</Text>
+        <View style={styles.uploadOptionContainer}>
+          <TouchableOpacity
+            style={[styles.uploadOptionButton, uploadOption === 'immediate' && styles.selectedOption]}
+            onPress={() => setUploadOption('immediate')}
+          >
+            <Text style={styles.uploadOptionText}>Tanpa Penundaan</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.uploadOptionButton, uploadOption === 'delayed10' && styles.selectedOption]}
+            onPress={() => setUploadOption('delayed10')}
+          >
+            <Text style={styles.uploadOptionText}>Penundaan 10 Detik</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.uploadOptionButton, uploadOption === 'delayed30' && styles.selectedOption]}
+            onPress={() => setUploadOption('delayed30')}
+          >
+            <Text style={styles.uploadOptionText}>Penundaan 30 Detik</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.submitButton} onPress={handleUpload}>
+          <Text style={styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 10,
+    paddingBottom: 30,
     backgroundColor: '#F5F5F5',
   },
   input: {
@@ -193,6 +259,25 @@ const styles = StyleSheet.create({
   },
   selectedText: {
     color: '#FFF',
+  },
+  uploadOptionContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    flexWrap: 'wrap',
+  },
+  uploadOptionButton: {
+    backgroundColor: '#E0E0E0',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    margin: 5,
+    borderRadius: 8,
+  },
+  selectedOption: {
+    backgroundColor: '#89AC46',
+  },
+  uploadOptionText: {
+    fontSize: 16,
+    color: '#333',
   },
   submitButton: {
     backgroundColor: '#89AC46',
